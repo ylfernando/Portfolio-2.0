0213aca2f00c4d8916763aab9ebb6f9dc14d7741
Fernando Freitas <ylfernando1@gmail.com>
2021-07-15 19:09:44 -0300

    Portfolio concluido e responsivo, faltando a parte do back-end

--- Assets/JS/scroll-suave.js
+++ Assets/JS/scroll-suave.js
@@ -11,6 +11,16 @@ $('.nav-scroll a[href^="#"]').click(function(e){
 	}, 500);
 });
 
+$('.nav-mobile a[href^="#"]').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.header').innerHeight(),
+			targetOffset = $(id).offset().top;
+	
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
 /*Scroll Footer*/
 
 $('.menu-footer a[href^="#"]').click(function(e){
